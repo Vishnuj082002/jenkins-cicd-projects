9692dc4d86e632f06ce224b14a425404c6cb56be
Vishnuj082002 <varadhanvishnu20@gmail.com>
2026-09-24 18:13:25 +0530

    Fix project name in API response

--- project-1-freestyle/index.js
+++ project-1-freestyle/index.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 3001;
 app.get("/", (req, res) => {
   res.status(200).json({
     status: "ok",
-    project: "project-1-freestyles APIs",
+    project: "project-1-freestyles API",
     message: "Welcome to Project 1",
   });
 });
