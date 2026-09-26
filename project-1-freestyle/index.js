ee88d93f7b9772d4bde66981e5d6c5b2c77f9575
Vishnuj082002 <varadhanvishnu20@gmail.com>
2026-09-26 17:00:25 +0530

    Update welcome message in index.js

--- project-1-freestyle/index.js
+++ project-1-freestyle/index.js
@@ -7,7 +7,7 @@ app.get("/", (req, res) => {
   res.status(200).json({
     status: "ok",
     project: "project-1-freestyles API",
-    message: "Welcome to Project 1",
+    message: "Welcome to vishnu aws",
   });
 });
 
